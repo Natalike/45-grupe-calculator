7015d5aa346905cbf382fea73a6e05bf08624f6c
Natalija_L <natalija.liske@gmail.com>
2023-06-21 08:36:39 +0300

    calc

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const equationDOM = document.querySelector('.equation');
+const buttonDOM = document.querySelector('.key');
+
+for (const buttonDOM os buttonDOM) {
+    buttonDOM.addEventListener('click', () => {
+        if (buttonDOM.innerText) {
+
+        }
+        equationDOM.innerText += buttonDOM.innerText;
+    });
+}
