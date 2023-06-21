071ac1035f442ba0febee4d11aa8a1e20d0daafb
Natalija_L <natalija.liske@gmail.com>
2023-06-21 10:04:41 +0300

    trinti

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
+/*
+1. susirasti tevini elementa (.calc)
+2. sugeneruoti visa HTML
+3. istatyti i tevini elementa
+4. susirasti visus mygtukus (.key)
+5. mygtukams uzdeti event listener'ius
+*/
+
 const equationDOM = document.querySelector('.equation');
-const buttonDOM = document.querySelector('.key');
+const buttonsDOM = document.querySelectorAll('.key');
 
-for (const buttonDOM of buttonDOM) {
+for (const buttonDOM of buttonsDOM) {
     buttonDOM.addEventListener('click', () => {
-        if (buttonDOM.innerText) {
+        const buttonValue = buttonDOM.innerText;
+        const currentEquationValue = equationDOM.innerText;
+        let updateEquationValue = currentEquationValue;
 
+        switch (buttonValue) {
+            case 'C':
+                updateEquationValue = '';
+                break;
+
+            case '<=':
+                updateEquationValue = currentEquationValue.slice(0, -1);
+                break;
+
+            default:
+                updateEquationValue += buttonValue;
+                break;
         }
-        equationDOM.innerText += buttonDOM.innerText;
+
+        equationDOM.innerText = updateEquationValue;
     });
 }
